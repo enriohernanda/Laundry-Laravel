f63431fb1fd10369cae78bb2bc87eb1785438b9a
enriohernanda <enriohernanda@gmail.com>
2025-11-17 15:16:31 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,15 @@ use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('admin.login');
-});
+})->name('login');
 
 Route::post('action-login', [LoginController::class, 'actionLogin'])->name('login.action');
+Route::get('logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/dashboard-admin', function () {
-    return view('admin.app');
-});
+    return view('admin.administrator.index');
+})->middleware(['auth', 'administrator']);
+
+Route::get('/dashboard-operator', function () {
+    return view('admin.operator.index');
+})->middleware(['auth', 'operator']);
